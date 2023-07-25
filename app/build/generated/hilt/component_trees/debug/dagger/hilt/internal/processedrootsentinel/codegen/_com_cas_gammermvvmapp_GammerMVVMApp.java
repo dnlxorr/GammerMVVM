28a926cae4fa6565f947c2d2440bbbaea72c2484
dtorres <isthisdani@gmail.com>
2023-07-25 15:36:20 -0500

    gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_cas_gammermvvmapp_GammerMVVMApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.cas.gammermvvmapp.GammerMVVMApp"
-)
-public final class _com_cas_gammermvvmapp_GammerMVVMApp {
-}
